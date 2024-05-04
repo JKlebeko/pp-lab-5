2f937570aa0833438571698a54ad81c6cfa77c04
jacek <jacek.klebeko@gmail.com>
2024-05-04 19:13:36 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,21 +1,21 @@
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
+import static company.utils.MathUtils.add;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        try {
-            Person person=new Person(135,"Jaca");
-        } catch (InvalidAgeException e) {
-            System.out.println(e.getMessage());
-        }
-
-
-
-
+        //try {
+         //   Person person=new Person(135,"Jaca");
+        //} catch (InvalidAgeException e) {
+        //    System.out.println(e.getMessage());
+        //}
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("Wiadomosc");
+
+        emailMessenger.sendMessage(+add(2,3)+" Wynik");
+
+
     }
     }
