01d126b93c14a65540e0dcb964129d34d9cbf700
jacek <jacek.klebeko@gmail.com>
2024-05-04 19:35:08 +0200

    zad3

--- Main.java
+++ Main.java
@@ -7,14 +7,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //try {
-         //   Person person=new Person(135,"Jaca");
-        //} catch (InvalidAgeException e) {
-        //    System.out.println(e.getMessage());
-        //}
-        EmailMessenger emailMessenger = new EmailMessenger();
 
-        emailMessenger.sendMessage(+add(2,3)+" Wynik");
+
+
+        final int B=10;
+        Person people[]=new Person[5];
+
+
+
+
+        try {
+            people[0] = new Person(44, "John Doo");
+            people[1] = new Person(48, "Jony WU");
+            people[2] = new Person(14, "Johnnson Whu");
+            people[3] = new Person(124, "Spejson Kowalski");
+            people[4] = new Person(69, "Wojtas jakis");
+
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        EmailMessenger emailMessenger = new EmailMessenger();
+        for (int i = 0; i < people.length; i++) {
+
+            emailMessenger.sendMessage(+add(B,people[i].getAge())+" Wiek");
+        }
+
 
 
     }
